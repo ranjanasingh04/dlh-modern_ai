b7c8fbf6c96f6be2044afb6f08fd8c8750e7b4b9
Ranjana Singh <ranjanasingh04@gmail.com>
2026-07-16 05:58:57 +0000

    Plot Correlation Heatmap

--- data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -12,25 +12,12 @@ def plot_correlation_heatmap(df):
     """
     plt.figure(figsize=(6, 5))
 
-    continuous_columns = [
-        "tenure",
-        "MonthlyCharges",
-        "TotalCharges"
-    ]
-
-    correlation_matrix = df[continuous_columns].corr()
-
-    sns.heatmap(
-        corr,
-        annot=True,
-        fmt=".2g",
-        cmap="coolwarm",
-        vmin=-1,
-        vmax=1,
-        square=True,
-        linewidths=0.5
-    )
-
-    plt.title("Correlation Matrix")
-    plt.tight_layout()
+    corr = df.corr(numeric_only=True)
+    sns.heatmap(corr,
+                vmin=-1,
+                vmax=1,
+                annot=True,
+                cmap='coolwarm'
+                )
+    plt.title('Correlation Matrix')
     plt.show()
