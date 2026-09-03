3847404abd5f88302279c6a11bd1b9f7f922ff58
Ranjana Singh <ranjanasingh04@gmail.com>
2026-09-03 20:49:03 +0000

    Unfreezing Layers

--- deep_learning/transfer_learning/2-unfreeze_top.py
+++ deep_learning/transfer_learning/2-unfreeze_top.py
@@ -5,10 +5,12 @@
 def unfreeze_top_layers(model, n_layers):
     """Unfreezes the last n layers of the model's base model.
     """
-    base_model = model.layers[0]
+    if n_layers <= 0:
+        raise ValueError("n must be a positive integer")
+    if n_layers > len(model.layers):
+        raise ValueError("n must be less than or equal "
+                         "to the number of layers in the base model")
 
-    for layer in base_model.layers[:-n_layers]:
-        layer.trainable = False
-
-    for layer in base_model.layers[-n_layers:]:
+    # Unfreeze the last N layers
+    for layer in model.layers[-n_layers:]:
         layer.trainable = True
